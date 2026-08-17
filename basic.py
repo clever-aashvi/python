e2843cef5a722ea0bf78392bc94db9da01969bac
clever-aashvi <cleverdude.sunil1@gmail.com>
2026-08-16 20:01:11 -0400

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello, world")
+
+# Printing my name
+print("My name is Aashvi")
+
+# Printing fun fact about myself
+print("I like to swim")
+
+# Preforming and print ing simple calculation
+print("The result of 3+5 is: ", 3 + 5)

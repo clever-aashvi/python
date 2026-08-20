0c5c27b61963b87291fd833b8e4eac4ef7e8e84e
clever-aashvi <cleverdude.sunil1@gmail.com>
2026-08-20 16:52:32 -0400

    Add files via upload

---
+++ world.py
@@ -0,0 +1,14 @@
+#print a sentence
+print("welcome to the world of programing!")
+
+#print a Number
+print(4)
+
+#next line after print
+print("hello \n")
+
+#pass multipul arguements for printing
+print("hello", 5)
+
+# end arguement of print statement 
+print("welcome to ", end= "*")

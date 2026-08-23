a696f5bc187f0080efed4fd864b1fb466b5af8d3
clever-aashvi <cleverdude.sunil1@gmail.com>
2026-08-23 19:33:32 -0400

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,13 @@
+# creating diff. variables
+x = 5
+y = "Jhon"
+print(x)
+print(y)
+
+codingal = "codeingal"
+print(codingal)
+
+#user input
+name = input("Enter your name: ")
+
+print("n/hello", name, "n/welcome to codingal")

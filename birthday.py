6dcc24861cce447fed28e476c16f8faa2e412e33
clever-aashvi <cleverdude.sunil1@gmail.com>
2026-08-27 17:14:18 -0400

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,15 @@
+#saving birthday
+b1 = "1/5/12"
+b2 = "4/4/13"
+b3 = "7/3/14"
+b4 = "10/2/15"
+b5 = "12/1/16"
+
+#printing bdays
+print("______________")
+print("friend 1's bday: ", b1)
+print("friend 2's bday: ", b2)
+print("friend 3's bday: ", b3)
+print("friend 4's bday: ", b4)
+print("friend 5's bday: ", b5)
+print("______________")

2b7676c908bfd7e7d095cb0d5b1377ac69c1b9b1
clever-aashvi <cleverdude.sunil1@gmail.com>
2026-08-28 16:39:13 -0400

    Add files via upload

---
+++ cast.py
@@ -0,0 +1,27 @@
+#assign. diff. variables
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+# print. diff. variables and data type
+print("Name :", name)
+print ("data type of name is", type(name))
+
+print("age :", age)
+print ("data type of name is", type(age))
+
+print("is_student :", is_student)
+print ("data type of name is", type(is_student))
+
+print("weight :", weight)
+print ("data type of name is", type(weight))
+
+# Type casting to convert the datatype of variables
+print("\n after type casting...")
+age = str(age)
+print(age)
+print("data typr of age is", type(age))
+weight = int(weight)
+print(weight)
+print("data typt of weight is", type(weight))

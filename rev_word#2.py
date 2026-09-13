d012650d65c56921aa0cd2bbd8e0dab25a625345
clever-aashvi <cleverdude.sunil1@gmail.com>
2026-09-13 19:39:26 -0400

    Add files via upload

---
+++ rev_word#2.py
@@ -0,0 +1,7 @@
+string = input("Please enter your own String : ")
+string2 = ('')
+for i in string:
+    string2 = i + string2
+
+print("\nThe Original String = ", string)
+print("The Reverced String = ", string2)

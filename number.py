f44f50256c1fe58937744e056716a2d85af03ad1
clever-aashvi <cleverdude.sunil1@gmail.com>
2026-09-16 18:26:33 -0400

    Add files via upload

---
+++ number.py
@@ -0,0 +1,7 @@
+
+base = float(input("Enter the base number: "))
+exponent = float(input("Enter the exponent (n): "))
+
+result = base ** exponent
+
+print(f"{base} raised to the power of {exponent} is: {result}")
